8a9f7b9da79da8d400db0d68774727f014ad2fda
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-12-27 09:48:36 +0200

    finish react functionality

--- 03-birthday-buddy/src/App.jsx
+++ 03-birthday-buddy/src/App.jsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+
+import data from './data';
+import Person from './Person';
+
 function App() {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+
+  function handleClick() {
+    setPeople([]);
+  }
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthdays Today </h3>
+        {people.map((person) => (
+          <Person {...person} key={person.id} />
+        ))}
+        <button onClick={handleClick} className="btn btn-block">
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
